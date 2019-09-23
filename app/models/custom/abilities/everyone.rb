7f3f9a87074616d5a2c8d2820e7cb1fa85e16d67
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-09-23 19:25:01 +0200

    Allow everyone to read campaings
    This will allow to access index and show campaigns action to all kind of users.

--- app/models/custom/abilities/everyone.rb
+++ app/models/custom/abilities/everyone.rb
@@ -32,6 +32,7 @@ module Abilities
       can [:read], Legislation::Question
       can [:read, :map, :share], Legislation::Proposal
       can [:search, :comments, :read, :create, :new_comment], Legislation::Annotation
+      can :read, Campaing
     end
   end
 end
