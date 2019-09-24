53ce5e80eb782bb67d5e4f98cd7dabc9ab666e2e
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-09-24 13:55:51 +0200

    Add scope to retrieve proposals from currently active campaigns

--- app/models/custom/proposal.rb
+++ app/models/custom/proposal.rb
@@ -3,5 +3,6 @@ require_dependency Rails.root.join("app", "models", "proposal").to_s
 Proposal.class_eval do
   belongs_to :campaing
 
+  scope :from_current_campaigns, -> { joins(:campaing).merge(Campaing.current) }
   validates :campaing, presence: true
 end
