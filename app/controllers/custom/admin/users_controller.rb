14887254f3eff62841b3e22b05a839378cbf4af6
Linux-wzlxqradcnsl01-root <root@wzlxqradcnsl01.worldbank.org>
2019-09-25 04:52:57 -0400

    Option to download users

---
+++ app/controllers/custom/admin/users_controller.rb
@@ -0,0 +1,13 @@
+class Admin::UsersController < Admin::BaseController
+  load_and_authorize_resource
+
+  def index
+    @users = User.by_username_email_or_document_number(params[:search]) if params[:search]
+    @users = @users.page(params[:page])
+    respond_to do |format|
+      format.html
+      format.js
+      format.csv
+    end
+  end
+end
