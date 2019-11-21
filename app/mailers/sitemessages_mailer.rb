173ea193a9af5f0de088e705c56aaaaa9c18dc5c
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-11-21 11:49:17 +0100

    Add sitemessage model as instance available at email templates

--- app/mailers/sitemessages_mailer.rb
+++ app/mailers/sitemessages_mailer.rb
@@ -1,5 +1,6 @@
 class SitemessagesMailer < ApplicationMailer
-  def contact_email(sitemessage) 
+  def contact_email(sitemessage)
+    @sitemessage = sitemessage
     mail(to: 'sbolleddula@worldbankgroup.org', subject: 'Welcome to My Awesome Site')
   end
 end
