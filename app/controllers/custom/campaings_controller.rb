6908c4920a081dc8dedfe4fa9eefafff433b06ce
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-05-04 17:53:27 +0530

    get the list of winning proposal and a poll

--- app/controllers/custom/campaings_controller.rb
+++ app/controllers/custom/campaings_controller.rb
@@ -2,7 +2,7 @@ class CampaingsController < ApplicationController
   load_and_authorize_resource
   def show
     @all_proposals = Proposal.where(campaing_id: @campaing.id)
-    @winning_proposal = Proposal.find(@campaing.proposal_id)
-    @poll = Poll.find(@campaing.poll_id)
+    @winning_proposal = Proposal.where(id: @campaing.proposal_id)
+    @poll = Poll.where(id: @campaing.poll_id)
   end
 end
