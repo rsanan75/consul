a5a8163dd9597705ae9dbda3161839dc0ff58530
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-05-04 22:21:19 +0530

    winning proposal

--- app/controllers/custom/campaings_controller.rb
+++ app/controllers/custom/campaings_controller.rb
@@ -2,10 +2,9 @@ class CampaingsController < ApplicationController
   load_and_authorize_resource
   def show
     @all_proposals = Proposal.where(campaing_id: @campaing.id)
-    @winning_proposals = []
     @all_proposals.each do |proposal|
       if proposal.id == @campaing.proposal_id
-        @winning_proposals += proposal
+        @winning_proposal = proposal
       end
     end
   end
