92f085c514f5ebc376ab5506694dd36962131d3b
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-05-04 22:53:50 +0530

    campaing winning proposal

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -12,6 +12,10 @@ ProposalsController.class_eval do
     end
     if params[:retired].present?
       @campaigns = Campaing.where(":date >= ends_at", date: Time.current).order(ends_at: :desc)
+      @winning_proposals = Hash.new
+      @campaings.each do |campaing|
+        @winning_proposals[campaing.id] = Proposal.where(id: campaing.proposal_id)
+      end
     else 
       @campaigns = Campaing.current.order(ends_at: :desc)
     end
