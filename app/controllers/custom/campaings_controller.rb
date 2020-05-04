a4c67bacd0451e32c7e1d6eeb03734ed8fbe8e9a
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-05-04 17:30:47 +0530

    list all proposals

--- app/controllers/custom/campaings_controller.rb
+++ app/controllers/custom/campaings_controller.rb
@@ -1,6 +1,8 @@
 class CampaingsController < ApplicationController
   load_and_authorize_resource
   def show
-    @all_proposals = Proposal.where(campaing_id: params(:id))
+    @all_proposals = Proposal.where(campaing_id: @campaing.id)
+    @winning_proposal = Proposal.where(id: @campaing.proposal_id)
+    @poll = Poll.where(id: @campaing.poll_id)
   end
 end
