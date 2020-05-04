b1657234eb3946cfdd720fd8f705e6bbc6f44670
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-05-04 18:14:38 +0530

    trying to get the results of winning proposal and poll

--- app/controllers/custom/campaings_controller.rb
+++ app/controllers/custom/campaings_controller.rb
@@ -2,7 +2,7 @@ class CampaingsController < ApplicationController
   load_and_authorize_resource
   def show
     @all_proposals = Proposal.where(campaing_id: @campaing.id)
-    @winning_proposal = Proposal.find(@campaing.proposal_id).load
-    @poll = Poll.find(@campaing.poll_id).load
+    @winning_proposal = Proposal.find(@campaing.proposal_id).values
+    @poll = Poll.find(@campaing.poll_id).values
   end
 end
