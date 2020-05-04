9234dec901c0c86e87987c0e9460fc3b654927b5
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-05-04 16:19:52 +0530

    campaing parameteres

--- app/controllers/custom/campaings_controller.rb
+++ app/controllers/custom/campaings_controller.rb
@@ -1,14 +1,3 @@
 class CampaingsController < ApplicationController
   load_and_authorize_resource
-
-
-  def index
-    if params[:retired].present?
-      @poll = Campaing.poll_id
-      @proposal = Campaing.proposal_id
-    else 
-      
-    end
-  end
-  
 end
