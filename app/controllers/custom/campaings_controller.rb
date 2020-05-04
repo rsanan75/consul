c46a3d6aece241247e9e97b06b01adc7628bbac5
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-05-04 16:04:38 +0530

    adding winning proposal to campaing

--- app/controllers/custom/campaings_controller.rb
+++ app/controllers/custom/campaings_controller.rb
@@ -1,3 +1,9 @@
 class CampaingsController < ApplicationController
   load_and_authorize_resource
+  if params[:retired].present?
+    @poll = Campaing.poll
+    @proposal = Campaing.proposal
+  else 
+    
+  end
 end
