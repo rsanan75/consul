8c5044e3fbc435d2059e9724ccc5c0cb60f020f1
Linux-wzlxqradcnsl01-root <root@wzlxqradcnsl01.worldbank.org>
2020-05-05 14:10:33 -0400

    added translatable fields

--- db/migrate/20200505125258_add_proposal_winningproposaltext_to_campaings.rb
+++ db/migrate/20200505125258_add_proposal_winningproposaltext_to_campaings.rb
@@ -1,13 +1,16 @@
 class AddProposalWinningproposaltextToCampaings < ActiveRecord::Migration[5.0]
-  def change
-	  add_reference :campaings, :proposal, foreign_key: true
-  end
   def self.up
+    change_table :campaings do |t|
+      t.references :proposal, foreign_key: true
+    end
     change_table(:campaing_translations) do |t|
       t.text :win_proposal_text
     end
   end
   def self.down
+    change_table :campaings do |t|
+      t.remove_references :proposal, foreign_key: true
+    end
     remove_column :campaing_translations, :win_proposal_text
   end
 end
