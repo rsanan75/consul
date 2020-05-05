3715d5d1e3092aafb7a5322607b48fd12cfb554d
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-05-05 23:15:22 +0530

    introducing translatable fields

--- db/migrate/20200505125258_add_proposal_winningproposaltext_to_campaings.rb
+++ db/migrate/20200505125258_add_proposal_winningproposaltext_to_campaings.rb
@@ -1,6 +1,11 @@
 class AddProposalWinningproposaltextToCampaings < ActiveRecord::Migration[5.0]
   def change
-	  add_column :campaings, :win_proposal_text, :text
 	  add_reference :campaings, :proposal, foreign_key: true
+  end
+  def up
+    Campaing.add_translation_fields! :win_proposal_text => :text
+  end
+  def down
+    remove_column :campaing_translations, :win_proposal_text
   end
 end
