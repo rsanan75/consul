797b1404f68d97e05eb158e2250cd934c782c52c
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-05-09 18:53:13 +0530

    order proposals acording to votes

--- app/controllers/custom/campaings_controller.rb
+++ app/controllers/custom/campaings_controller.rb
@@ -1,7 +1,7 @@
 class CampaingsController < ApplicationController
   load_and_authorize_resource
   def show
-    @all_proposals = Proposal.where(campaing_id: @campaing.id)
+    @all_proposals = Proposal.where(campaing_id: @campaing.id).order(cached_votes_up: :desc)
     @all_proposals.each do |proposal|
       if proposal.id == @campaing.proposal_id
         @winning_proposal = proposal
