d0f6d905e56c01cf26a42884b4ae84fb03dd1579
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-11-27 20:05:46 +0530

    removing pager

--- app/controllers/custom/admin/users_controller.rb
+++ app/controllers/custom/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < Admin::BaseController
 
   def index
     @users = User.by_username_email_or_document_number(params[:search]) if params[:search]
-    @users = @users.page(params[:page])
+    
     respond_to do |format|
       format.html
       format.js
