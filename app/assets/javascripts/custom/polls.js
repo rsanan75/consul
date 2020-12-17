93dcb3c52668bc5ef6e5a56a834c4ff345f7702d
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2020-12-17 18:42:26 +0530

    added a button to ajax submit url

---
+++ app/assets/javascripts/custom/polls.js
@@ -0,0 +1,53 @@
+(function() {
+  "use strict";
+  App.Polls = {
+    generateToken: function() {
+      var strings;
+      strings = Array.apply(null, {
+        length: 6
+      }).map(function() {
+        return Math.random().toString(36).substr(2); // remove `0.`
+      });
+      return strings.join("").substring(0, 64);
+    },
+    replaceToken: function(token) {
+      $(".js-question-answer").each(function() {
+        var token_param;
+        if (!this.search && jQuery(this).data('answer-url')) {
+          token_param = jQuery(this).data('answer-url').slice(-6);
+          if (token_param === "token=") {
+            jQuery(this).data('answer-url') = jQuery(this).data('answer-url') + token;
+          }
+        } else {
+          token_param = this.search.slice(-6);
+          if (token_param === "token=") {
+            this.href = this.href + token;
+          }
+        }
+      });
+    },
+    initialize: function() {
+      var token;
+      token = App.Polls.generateToken();
+      App.Polls.replaceToken(token);
+      $(".zoom-link").on("click", function(event) {
+        var answer;
+        answer = $(event.target).closest("div.answer");
+
+        if ($(answer).hasClass("medium-6")) {
+          $(answer).removeClass("medium-6");
+          $(answer).addClass("answer-divider");
+          if (!$(answer).hasClass("first")) {
+            $(answer).insertBefore($(answer).prev("div.answer"));
+          }
+        } else {
+          $(answer).addClass("medium-6");
+          $(answer).removeClass("answer-divider");
+          if (!$(answer).hasClass("first")) {
+            $(answer).insertAfter($(answer).next("div.answer"));
+          }
+        }
+      });
+    }
+  };
+}).call(this);
