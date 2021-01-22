bb37f47db99d69042c4ae6a0caf8aae21414cd40
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2021-01-22 21:47:13 +0530

    added recaptcha proxy

--- app/controllers/custom/sitemessages_controller.rb
+++ app/controllers/custom/sitemessages_controller.rb
@@ -1,5 +1,9 @@
 class SitemessagesController < ApplicationController
+  require 'recaptcha.rb'
+
+  before_action :verify_recaptcha, only: [:create]
   skip_authorization_check :only => [:create, :new]
+
   def new
     @sitemessage = Sitemessage.new
   end
@@ -7,7 +11,7 @@ class SitemessagesController < ApplicationController
   def create
     @sitemessage = Sitemessage.new(sitemessage_params)
     
-    if verify_recaptcha && @sitemessage.save
+    if @sitemessage.save
       SitemessagesMailer.contact_email_all(@sitemessage.name, @sitemessage.email, @sitemessage.message).deliver_later
       redirect_to root_path, notice: t("customtext.sitemessages.success_message")
     else
@@ -19,5 +23,20 @@ class SitemessagesController < ApplicationController
   def sitemessage_params
     params.require(:sitemessage).permit(:name,:email,:message)
   end
-  
+
+  def verify_recaptcha
+    response = Recaptcha.verify(params)
+    session[:sitemessage] = params[:sitemessage].except(:name,:email,:message, :remoteip)
+    if response.code == 200
+      if response['success']
+        flash[:notice] = "Recaptcha verification successful."
+      else
+      redirect_to new_sitemessage_path(sitemessage: params[:sitemessage]),
+      alert: "Recaptcha verification error."
+      end
+    else
+      redirect_to new_sitemessage_path(sitemessage: params[:sitemessage]),
+      alert: "HTTP connection error."
+    end
+  end
 end
