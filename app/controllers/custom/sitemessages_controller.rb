dde87edf0813d5f12255fa5283cfdc1ccd3382e9
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2021-01-22 22:23:54 +0530

    controller debug

--- app/controllers/custom/sitemessages_controller.rb
+++ app/controllers/custom/sitemessages_controller.rb
@@ -1,21 +1,20 @@
 class SitemessagesController < ApplicationController
-  include Recaptcha::Verify
-
-  before_action :verify_recaptcha, only: [:create]
   skip_authorization_check :only => [:create, :new]
-
   def new
     @sitemessage = Sitemessage.new
   end
 
   def create
     @sitemessage = Sitemessage.new(sitemessage_params)
-    
-    if @sitemessage.save
-      SitemessagesMailer.contact_email_all(@sitemessage.name, @sitemessage.email, @sitemessage.message).deliver_later
-      redirect_to root_path, notice: t("customtext.sitemessages.success_message")
+    if verify_recaptcha(model: @sitemessage)
+      if @sitemessage.save
+        SitemessagesMailer.contact_email_all(@sitemessage.name, @sitemessage.email, @sitemessage.message).deliver_later
+        redirect_to root_path, notice: t("customtext.sitemessages.success_message")
+      else
+        render :new
+      end
     else
-      render :new
+      flash.now[:alert] = t("Error on the recaptcha")
     end
   end
   private
@@ -23,15 +22,5 @@ class SitemessagesController < ApplicationController
   def sitemessage_params
     params.require(:sitemessage).permit(:name,:email,:message)
   end
-
-  def verify_recaptcha
-    response = Recaptcha.verify(params)
-    session[:sitemessage] = params[:sitemessage].except(:name,:email,:message, :remoteip)
-    if verify_recaptcha
-    else
-      redirect_to new_sitemessage_path(sitemessage: params[:sitemessage]),
-      alert: :recaptcha_error
-    end
-   
-  end
+  
 end
