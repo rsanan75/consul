7909ae7e72b2c317abb4e90da77544e8fd18f21a
Sambasiva Rao Bolleddula <sbolleddula@worldbankgroup.org>
2021-01-22 21:24:07 +0530

    added recaptcha proxy

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
   config.site_key  = ENV['RECAPTCHA_PUBLIC_KEY']
   config.secret_key  = ENV['RECAPTCHA_PRIVATE_KEY']
-  config.proxy = 'https://www.google.com/recaptcha/api/verify'
+  config.proxy = 'https://www.google.com/recaptcha/api/siteverify'
 end
