063dda25f067c1570c05c07c99a454cd32da2fc3
Linux-wzlxqradcnsl02-root <root@wzlxqradcnsl02.worldbank.org>
2021-03-08 02:18:48 -0500

    captcha error fix

--- app/controllers/custom/sitemessages_controller.rb
+++ app/controllers/custom/sitemessages_controller.rb
@@ -5,7 +5,10 @@ class SitemessagesController < ApplicationController
   end
 
   def create
+	  #abort params.inspect
+	 status = verify_google_recptcha('6LcTZDcaAAAAAKSJqrzdGH1Okn5RqciXnQEEvJUS',params["g-recaptcha-response"])
     @sitemessage = Sitemessage.new(sitemessage_params)
+    
     if @sitemessage.save and verify_recaptcha
       SitemessagesMailer.contact_email_all(@sitemessage.name, @sitemessage.email, @sitemessage.message).deliver_later
       redirect_to root_path, notice: t("customtext.sitemessages.success_message")
@@ -14,7 +17,13 @@ class SitemessagesController < ApplicationController
     end
   end
   private
-
+  
+  def verify_google_recptcha(secret_key,response)
+	    status = 'curl "https://www.google.com/recaptcha/api/siteverify?secret=#{secret_key}&response=#{response}"'
+	        logger.info "---------------status ==> #{status}"
+		    hash = JSON.parse(status)
+		        hash["success"] == true ? true : false
+  end
   def sitemessage_params
     params.require(:sitemessage).permit(:name,:email,:message)
   end
